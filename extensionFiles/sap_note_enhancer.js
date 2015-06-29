76eb6fcd69a045e8cd9635d5bf456bf69fdec86b
Guilherme Dellagustin <dellagustin@gmail.com>
2015-06-30 00:10:57 +0200

    Refactoring for reuse of common post highlighting processing

--- extensionFiles/sap_note_enhancer.js
+++ extensionFiles/sap_note_enhancer.js
@@ -27,34 +27,5 @@ $( document ).ready(function() {
 
 	Prism.highlightAll();
 
-	forEachCorrectionInstruction(function(myElement) {
-		var newHtmlForElement = myElement.html();
-
-		// Build insertion and deletion DIVs
-		newHtmlForElement = replaceAll(
-			newHtmlForElement,
-			'<span class="token comment" spellcheck="true">*&gt;&gt;&gt;&gt; START OF DELETION &lt;&lt;&lt;&lt;&lt;',
-			'<div class="ci_deletion"><span class="token comment" spellcheck="true"><b>*&gt;&gt;&gt;&gt; START OF DELETION &lt;&lt;&lt;&lt;&lt;</b>'
-		);
-
-		newHtmlForElement = replaceAll(
-			newHtmlForElement,
-			'*&gt;&gt;&gt;&gt; END OF DELETION &lt;&lt;&lt;&lt;&lt;&lt;&lt;</span>',
-			'<b>*&gt;&gt;&gt;&gt; END OF DELETION &lt;&lt;&lt;&lt;&lt;&lt;&lt;</b></span></div>'
-		);
-
-		newHtmlForElement = replaceAll(
-			newHtmlForElement,
-			'<span class="token comment" spellcheck="true">*&gt;&gt;&gt;&gt; START OF INSERTION &lt;&lt;&lt;&lt;',
-			'<div class="ci_insertion"><span class="token comment" spellcheck="true"><b>*&gt;&gt;&gt;&gt; START OF INSERTION &lt;&lt;&lt;&lt;&lt;</b>'
-		);
-
-		newHtmlForElement = replaceAll(
-			newHtmlForElement,
-			'*&gt;&gt;&gt;&gt; END OF INSERTION &lt;&lt;&lt;&lt;&lt;&lt;</span>',
-			'<b>*&gt;&gt;&gt;&gt; END OF INSERTION &lt;&lt;&lt;&lt;&lt;&lt;</b></span></div>'
-		);
-
-		myElement.html(newHtmlForElement);
-	});
+	forEachCorrectionInstruction(commonCIPostProcessing);
 });
