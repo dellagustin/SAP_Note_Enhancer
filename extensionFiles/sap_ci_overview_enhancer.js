1cb9775d735733c80a95541d43def4b800c61887
Guilherme Dellagustin <dellagustin@gmail.com>
2015-06-30 00:01:15 +0200

    Added support to highlighting in the CI overview page

--- extensionFiles/sap_ci_overview_enhancer.js
+++ extensionFiles/sap_ci_overview_enhancer.js
@@ -1,5 +1,5 @@
 function forEachCorrectionInstruction(callback) {
-	$('div.urTxtStd > p').each(function(index) {
+	$('pre').each(function(index) {
 		var myElement = $( this );
 
 		if( myElement.text().search('KORREKTURANLEITUNG') >= 0 || myElement.text().search('Correction Inst.') >= 0)
@@ -12,17 +12,23 @@ function forEachCorrectionInstruction(callback) {
 $( document ).ready(function() {
 
 	forEachCorrectionInstruction(function(myElement) {
-		var newHtmlForElement = myElement.html();
+		myElement.addClass('language-abap');
 
-		newHtmlForElement = replaceAll(newHtmlForElement,	'<br>',	'\n');
+    var newHtmlForElement = myElement.html();
 
-		newHtmlForElement = '<div class="ci_block"><pre class="language-abap"><code>' + newHtmlForElement + '</code></pre><div>';
+    // Remove the two front spaces
+    newHtmlForElement = replaceAll(newHtmlForElement, '  *$*$', '*$*$');
+    newHtmlForElement = replaceAll(newHtmlForElement, '\n  ', '\n');
+
+    newHtmlForElement = '<code>' + newHtmlForElement + '</code>';
 
 		// Prevents Highlighting of non ABAP comments
     newHtmlForElement = replaceAll(newHtmlForElement,	'*\n&gt;&gt;&gt;',	'*\n</code><code class="language-none">&gt;&gt;&gt;');
 		newHtmlForElement = replaceAll(newHtmlForElement,	'&lt;&lt;&lt;\n\n*&amp;',	'&lt;&lt;&lt;</code><code class="language-abap">\n\n*&amp;');
 
 		myElement.html(newHtmlForElement);
+
+    myElement.wrap('<div class="ci_block"></div>')
 	});
 
 	Prism.highlightAll();
