2a202fcd580fd184dbecf82017a4f045d91baf60
Guilherme Dellagustin <dellagustin@gmail.com>
2015-07-22 00:21:25 +0200

    Add link to pre requisite CIs in CI headers
    In order to create this link an addional parameter called pakid
    is needed.
    We take this parameter from the existing link to the current
    CI(s) (first found is used).
    This seems to be the only way to get this parameter.
    It should be accurate in most cases, as it seems to be related to
    the Software Component.
    It may still fail in notes containing mixed Software Components.

--- extensionFiles/sap_note_enhancer.js
+++ extensionFiles/sap_note_enhancer.js
@@ -9,6 +9,29 @@ function forEachCorrectionInstruction(callback) {
 	});
 }
 
+function getPakID() {
+	var pakID = '';
+
+	// pakid is necessary to create the links to other correction instructions.
+	// the only way to get A (not THE) pakid is from the link to the current
+	// CI that is present in the note.
+	// This may not be the correct pakid for every CI, but as it seems to be
+	// related to software component, there is a big chance it is correct.
+	$('a').each(function(){
+		var linkElement = $(this);
+
+		var pakIdParam = linkElement.attr('href').match(/(_CPAKID|pv_pakid)=\d*/i);
+
+		pakIdParam[0] && (pakID = pakIdParam[0].match(/\d+/)[0]);
+
+		if(pakID !== '') {
+			return false;
+		}
+	});
+
+	return pakID;
+}
+
 $( document ).ready(function() {
 
 	forEachCorrectionInstruction(function(myElement) {
@@ -26,6 +49,30 @@ $( document ).ready(function() {
 	});
 
 	Prism.highlightAll();
+
+	// Look for the packid
+	var ciPakID = getPakID();
+
+	if(ciPakID !== '') {
+		// Add links to CIs in CI header
+		forEachCorrectionInstruction(function(myElement) {
+			var newHtmlForElement = myElement.html();
+
+			newHtmlForElement = newHtmlForElement.replace(/\*\$\s.*\$\*/g, function(headerLine) {
+				return headerLine.replace(/\d{10}\s\d{10}/, function(correctionInstruction) {
+					var ciNumbers = correctionInstruction.match(/\d{10}/g);
+
+					correctionInstruction =
+						'<a href="/sap/bc/bsp/spn/sno_corr/corr_inst_details.htm?_CINSTA=' + ciNumbers[0] +
+						'&_CALEID=' + ciNumbers[1] + '&_CPAKID=' + ciPakID + '">' + correctionInstruction + '</a>';
+
+					return correctionInstruction
+				});
+			});
+
+			myElement.html(newHtmlForElement);
+		});
+	}
 
 	forEachCorrectionInstruction(commonCIPostProcessing);
 });
