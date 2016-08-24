42894281fc4d035ccc4191c6508662c2945038be
Guilherme Dellagustin <dellagustin@gmail.com>
2016-08-24 12:08:21 +0200

    Fix for insertion/deletion highlighting on ONE Support Lauchpad
    In the new version of the one support launchpad the correction
    instruction is already in a <code> tag.
    
    When we wrap it again in a code tag it causes an exception in
    Prism that prevents some code from running.
    
    This caused the problem with highlighting the insertions and
    deletions.

--- extensionFiles/sap_ci_launchpad_enhancer.js
+++ extensionFiles/sap_ci_launchpad_enhancer.js
@@ -9,8 +9,6 @@ $( document ).on('DOMNodeInserted', 'pre', function(eventObject) {
 		var newHtmlForElement = thisElement.html();
 		newHtmlForElement = replaceAll(newHtmlForElement,	'<br>',	'\n');
 
-		newHtmlForElement = '<code>' + newHtmlForElement + '</code>';
-
 		// Prevents Highlighting of non ABAP comments
 		newHtmlForElement = replaceAll(newHtmlForElement, '*\n&gt;&gt;&gt;', '*\n</code><code class="language-none">&gt;&gt;&gt;');
 		newHtmlForElement = replaceAll(newHtmlForElement, '&lt;&lt;&lt;\n\n*&amp;', '&lt;&lt;&lt;</code><code class="language-abap">\n\n*&amp;');
